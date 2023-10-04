4349010090e496b4679148ea82cf386fe19bb2e2
jeromehardaway <jerome@vetswhocode.io>
2023-10-04 13:44:53 -0400

    add pizza bot code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,37 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Initialize a session state for the chat history
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Title
+st.title("PizzaBot")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# User Input
+user_input = st.text_input("You: ")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# When the user presses enter
+if user_input:
 
+    # Add the user input to chat history
+    st.session_state.chat_history.append({"user": user_input})
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # Bot Logic (You can replace this with more advanced logic or NLP)
+    if "menu" in user_input.lower():
+        bot_reply = "We have Margherita, Vegan, and Pepperoni pizzas."
+    elif "order" in user_input.lower():
+        bot_reply = "Great! What type of pizza would you like to order?"
+    elif "hello" in user_input.lower():
+        bot_reply = "Hello! How can I assist you today?"
+    else:
+        bot_reply = "I'm sorry, I don't understand."
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Add the bot's reply to chat history
+    st.session_state.chat_history.append({"bot": bot_reply})
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display Chat History
+for chat in st.session_state.chat_history:
+    if "user" in chat:
+        st.write(f"You: {chat['user']}")
+    else:
+        st.write(f"PizzaBot: {chat['bot']}")
